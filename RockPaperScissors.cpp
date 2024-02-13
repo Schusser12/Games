bcabc61c338e8987213170606047b01b725540d6
Schusser12 <114158870+Schusser12@users.noreply.github.com>
2024-02-13 04:28:25 +0100

    Update RockPaperScissors.cpp

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
 int main() {
-    std::string choices[] = { "Rock", "Paper", "Scissors" };
+    string choices[] = { "Rock", "Paper", "Scissors" };
     int numOfChoices = sizeof(choices) / sizeof(choices[0]);
 
     // Generate a random number between 0 and 2
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    srand(static_cast<unsigned int>(time(0)));
 
     char playAgain;
     do {
-        int computerChoice = std::rand() % numOfChoices;
+        int computerChoice = rand() % numOfChoices;
 
         int playerChoice;
 
-        std::cout << "*** Rock, Paper, Scissors ***\n\n";
+        cout << "*** Rock, Paper, Scissors ***\n\n";
 
         // Get the player's choice
-        std::cout << "Choose your move:\n";
-        std::cout << "1. Rock\n";
-        std::cout << "2. Paper\n";
-        std::cout << "3. Scissors\n";
-        std::cin >> playerChoice;
+        cout << "Choose your move:\n";
+        cout << "1. Rock\n";
+        cout << "2. Paper\n";
+        cout << "3. Scissors\n";
+        cin >> playerChoice;
+		
 
         playerChoice--; // Adjusting for 0-based indexing
 
         // Validate the player's choice
         if (playerChoice >= 0 && playerChoice < numOfChoices) {
-            std::cout << "You chose: " << choices[playerChoice] << "\n";
-            std::cout << "The computer chose: " << choices[computerChoice] << "\n\n";
+            cout << "You chose: " << choices[playerChoice] << "\n";
+            cout << "The computer chose: " << choices[computerChoice] << "\n\n";
 
             // Determine the winner
             int result = (playerChoice - computerChoice + numOfChoices) % numOfChoices;
 
             if (result == 0) {
-                std::cout << "It's a tie!\n";
+                cout << "It's a tie!\n";
             } else if (result <= numOfChoices / 2) {
-                std::cout << "Congratulations! You win!\n";
+                cout << "Congratulations! You win!\n";
             } else {
-                std::cout << "Sorry! You lose!\n";
+                cout << "Sorry! You lose!\n";
             }
         } else {
-            std::cout << "Invalid choice.\n";
+            cout << "Invalid choice.\n";
         }
 
-        std::cout << "Do you want to play again? (Y/N): ";
-        std::cin >> playAgain;
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> playAgain;
 
         playAgain = tolower(playAgain);
 
